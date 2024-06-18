abd254d188858bdf820e31b2ab99101163111505
Kiernan2 <kiernan1994@gmail.com>
2024-06-18 14:06:30 -0700

    Now working react project with bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <header>
+      <div class="row">
+        <div class="col-6">
+          <button className="px-2">Home</button>  <button>Notifications</button>  <button>Messages</button>
+        </div>
+        <div className="col-6 text-end">
+          Search Tweet
+        </div>
+      </div>
+    </header>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
