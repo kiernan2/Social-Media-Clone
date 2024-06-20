029fe5d5e27405fbe2bd72c2ad6b1a3bef47122c
Kiernan2 <kiernan1994@gmail.com>
2024-06-20 14:47:29 -0700

    Header quality improvement

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from "react";
 
 function Header(){
   return (
-    <header>
+    <header class="container">
       <div class="row">
-        <div class="col-6">
-          <button className="px-2">Home</button>  <button>Notifications</button>  <button>Messages</button>
+        <div class="col">
+          <button class="rounded-0 border border-dark btn btn-light">Home</button>
+          <button class="rounded-0 border border-dark btn btn-light">Notifications</button>
+          <button class="rounded-0 border border-dark btn btn-light">Messages</button>
         </div>
-        <div className="col-6 text-end">
-          Search Tweet
+        <div className="col text-end">
+          <input class="rounded-pill border border-primary" type="text" placeholder="Search" aria-label="Search"/>
+          <input class="rounded-pill border border-primary" type="text" placeholder="Tweet" aria-label="Tweet"/>
         </div>
       </div>
     </header>
