9386b5e93442bb5a65b80b0b38fceb6afcab12e5
Kiernan2 <kiernan1994@gmail.com>
2024-06-25 15:56:13 -0700

    Added Body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./components/Header.js";
+import Body from "./components/Body.js"
 
 function App(){
   return (
     <React.Fragment>
       <Header />
-      
+      <Body />
     </React.Fragment>
   );
 }
